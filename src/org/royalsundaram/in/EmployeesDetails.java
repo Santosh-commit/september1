38a8d16b1a2d276ec0cded827462b8bc66ae2d27
Santosh-commit <santoshkr4love@gmail.com>
2022-08-31 21:47:51 +0530

    i pushed my project

---
+++ src/org/royalsundaram/in/EmployeesDetails.java
@@ -0,0 +1,25 @@
+package org.royalsundaram.in;
+
+public class EmployeesDetails {
+	
+	
+	private void eCommerce() {
+
+		
+		System.out.println("amazon");
+		
+	}
+private void eCommerceOne() {
+
+	
+	System.out.println("flipkart");
+}
+
+public static void main(String[] args) {
+	
+	EmployeesDetails e = new EmployeesDetails();
+	e.eCommerce();
+	e.eCommerceOne();
+	
+}
+}
